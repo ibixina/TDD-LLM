5266b13c93825b658f4eaf0017efd167fab5091c
Shishir <theshishir01@gmail.com>
2025-05-14 19:53:34 -0500

    genai setup

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
-import sys
-args = sys.argv
+from google import genai
+from dotenv import load_dotenv
+import os
 
-def main():
-    pass
-
-if __name__=="__main__":
-    main()
+load_dotenv()
 
 
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+)
+print(response.text)
