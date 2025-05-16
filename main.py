2d597a4c798a04d6cabd6796e4705c07a9e997bd
Shishir <theshishir01@gmail.com>
2025-05-15 19:36:17 -0500

    testing foramts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import os
 
@@ -8,6 +9,9 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.0-flash", contents="write a code to print fiboncaci sequence in python",
+    config=types.GenerateContentConfig(
+        system_instruction="You are a code generation AI assistant. You will be given instructions, you will generate the code in the best and optimal way. Validity and correctness of the code is most important. Then you will be given the response to the code, and you shall correct the code if needed to generate the correct output. Only output the code. The code needs to be able to be plugged in and run without any changes. Do not include any extra information that might break the code -- example: example usage, description, etc."
+    )
 )
 print(response.text)
