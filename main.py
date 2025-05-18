8b3077e9c3d35049321abee6d4c0164e005d54c0
nao <theshishir01@gmail.com>
2025-05-18 14:26:17 -0500

    refactoring

--- main.py
+++ main.py
@@ -47,16 +47,19 @@ def parse_code(code):
     code = code.replace("```", "")
     return code
 
+def prompt_LLM(prompt):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=prompt,
+        config=types.GenerateContentConfig(
+            system_instruction="You are a code generation AI assistant. You will be given instructions, you will generate the code in the best and optimal way. Validity and correctness of the code is most important. Then you will be given the response to the code, and you shall correct the code if needed to generate the correct output. Only output the code. The code needs to be able to be plugged in and run without any changes. Do not include any extra information that might break the code -- example: example usage, description, etc. Do not include any commands that require user interaction."
+        )
+    )
+    return response
+
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents="write a code to print fiboncaci sequence in python",
-    config=types.GenerateContentConfig(
-        system_instruction="You are a code generation AI assistant. You will be given instructions, you will generate the code in the best and optimal way. Validity and correctness of the code is most important. Then you will be given the response to the code, and you shall correct the code if needed to generate the correct output. Only output the code. The code needs to be able to be plugged in and run without any changes. Do not include any extra information that might break the code -- example: example usage, description, etc. Do not include any commands that require user interaction."
-    )
-)
-print(response)
+response = prompt_LLM("Write a python script that prints 'Hello World'.")
 
 with open("test.py", "w") as f:
     code = parse_code(response.text)
